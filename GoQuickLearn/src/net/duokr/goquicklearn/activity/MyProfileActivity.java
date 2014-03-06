1e757f4ef3a7d6229e9911ed81bd8fdad916b8b2
Jemy <jemygraw@gmail.com>
2014-03-06 15:10:01 +0800

    Fix the bug of navigate back error under lower versions like 2.1

--- GoQuickLearn/src/net/duokr/goquicklearn/activity/MyProfileActivity.java
+++ GoQuickLearn/src/net/duokr/goquicklearn/activity/MyProfileActivity.java
@@ -2,7 +2,6 @@ package net.duokr.goquicklearn.activity;
 
 import net.duokr.goquicklearn.R;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
@@ -31,7 +30,7 @@ public class MyProfileActivity extends ActionBarActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
+			super.onBackPressed();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
