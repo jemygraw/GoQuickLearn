3a03e89966486a74abcbf9115c9ad2f114cd1809
Jemy <jemygraw@gmail.com>
2014-03-10 22:11:10 +0800

    增加有米广告模块，可以enable或者disable。

---
+++ GoQuickLearn/src/net/duokr/goquicklearn/config/YoumiAdConfig.java
@@ -0,0 +1,8 @@
+package net.duokr.goquicklearn.config;
+
+public class YoumiAdConfig {
+	public static final boolean YOUMI_AD_ENABLED = true;
+	public static final String APP_ID = "4823d3889c06c7af";
+	public static final String APP_KEY = "5651db1c4d19af08";
+
+}
